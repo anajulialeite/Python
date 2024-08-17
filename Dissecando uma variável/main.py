56fce1892118e86fa67d88239200089b4094fd6b
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-08-17 20:58:32 -0300

    Add files via upload

---
+++ Dissecando uma variável/main.py
@@ -0,0 +1,13 @@
+a = input("Digite algo: ")
+#O (a) significa métodos
+print("O tipo primitivo desse valor é ", type(a))
+print("Só tem espaço? ", a.isspace())
+print("É um número? ", a.isnumeric())
+print("É alfabético? ", a.isalpha())
+print("É alfanumérico? ", a.isalnum())
+print("Está em Maiúsculas? ", a.isupper())
+print("Está em minúsculas? ", a.islower())
+print("Está capitalizada? ", a.istitle())
+
+
+
