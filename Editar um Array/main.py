43ea9a5488c571ab330f099b680fec3c630476a4
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-08-17 19:18:37 -0300

    Add files via upload

---
+++ Editar um Array/main.py
@@ -0,0 +1,6 @@
+frutas = ["Laranja", "Acerola", "Abacate"]
+print(frutas)
+frutas[0] = "Coco"
+print(frutas)
+frutas[1] = "Amora"
+print(frutas)
---
+++ Remover um Array/main.py
@@ -0,0 +1,10 @@
+cores = ["Azul", "Verde", "Vermelho"]
+print(cores)
+"O pop serve para excluir um elemento da minha lista"
+item = cores.pop()
+"Ficou só Azul e Verde"
+print (cores)
+"Ficou somente a última cor da lista"
+print(item)
+"também podemos fazer o seguinte, utilizar apenas cores.pop() e print(cores) e vai aparecer somente as duas primeiras cores da lista, Azul e Verde."
+"Se for excluir uma lista em branco, ele vai mostrar uma excessão em python, vai mostrar um erro que deve ser corrigido"
