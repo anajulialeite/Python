041c82e8e14ad1e60345b93d99bcf312eeae7089
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-08-28 14:39:18 -0300

    Update main.py

--- Ana lisando Triângulo/main.py
+++ Ana lisando Triângulo/main.py
@@ -7,4 +7,4 @@ r3 = float(input("Terceiro seguimento "))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print("Os segmentos acima PODEM formar um triângulo!")
 else:
-    print("Os segmentos acima NÃO podem formar um triângulo!")
+    print("Os segmentos acima NÃO PODEM formar um triângulo!")
